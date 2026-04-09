4d3f938fb020a7ef6a57e51d0a0bfa824d165d3b
meddeb22 <meddebimtiezz@gmail.com>
2026-04-09 12:22:58 +0200

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LogisticRegression
 import joblib   
 
 # Charger les données                                        
-data = pd.read_csv('data/train_data.csv')     
+data = pd.read_csv('train_data.csv')     
 
 
 X = data[['Age', 'Account_Manager', 'Years', 'Num_Sites']] 
